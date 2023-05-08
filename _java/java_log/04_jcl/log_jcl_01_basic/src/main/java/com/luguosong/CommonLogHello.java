84ff93267f9f9d56fa74d6379589c8ff388cf2a5
luguosong <1054595718@qq.com>
2023-05-08 08:50:19 +0800

    更新 jcl示例代码

--- _java/java_log/04_jcl/log_jcl_01_basic/src/main/java/com/luguosong/CommonLogHello.java
+++ _java/java_log/04_jcl/log_jcl_01_basic/src/main/java/com/luguosong/CommonLogHello.java
@@ -3,17 +3,28 @@ package com.luguosong;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import java.io.IOException;
+
 /**
  * 默认情况下，会使用JUL框架
  *
  * @author luguosong
  */
 public class CommonLogHello {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+
+
         // 1.创建日志记录器对象
         Log log = LogFactory.getLog(CommonLogHello.class);
 
         // 2.日志记录输出
-        log.info("info信息");
+        log.trace("This is a TRACE message");
+        log.debug("This is a DEBUG message");
+        log.info("This is an INFO message");
+        log.warn("This is a WARN message");
+        log.error("This is an ERROR message");
+        log.fatal("This is a FATAL message");
+
     }
 }
