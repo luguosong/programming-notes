1e89bdc8f9b6bdeaaa271273bf54cb7b76874dd4
luguosong <1054595718@qq.com>
2023-09-11 14:44:48 +0800

    vue 笔记更新
    Signed-off-by: luguosong <1054595718@qq.com>

--- _front_end/04_vue/src/stores/counter.js
+++ _front_end/04_vue/src/stores/counter.js
@@ -1,7 +1,7 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed } from "vue"
+import { defineStore } from "pinia"
 
-export const useCounterStore = defineStore('counter', () => {
+export const useCounterStore = defineStore("counter", () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
   function increment() {
