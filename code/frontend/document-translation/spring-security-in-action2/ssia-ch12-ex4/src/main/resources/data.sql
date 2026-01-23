ae564b19400c4236f45d60d2253c9aafc178f7c1
luguosong <1054595718@qq.com>
2026-01-23 14:10:55 +0800

    ✨ feat: 在 `index.md` 文件中新增了 Mobx 相关内容，提升了文档的完整性与可读性。增加了空行以改善文件的可读性，增强了用户体验。

--- code/frontend/document-translation/spring-security-in-action2/ssia-ch12-ex4/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT IGNORE INTO `ssia-ch12-ex4`.`product` (`id`, `name`, `owner`) VALUES ('1', 'beer', 'nikolai');
-INSERT IGNORE INTO `ssia-ch12-ex4`.`product` (`id`, `name`, `owner`) VALUES ('2', 'candy', 'nikolai');
-INSERT IGNORE INTO `ssia-ch12-ex4`.`product` (`id`, `name`, `owner`) VALUES ('3', 'chocolate', 'julien');
--- code/frontend/document-translation/spring-security-in-action2/ssia-ch12-ex5/src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `ssia-ch12-ex5`.`product` (
-     `id` INT NOT NULL AUTO_INCREMENT,
-     `name` VARCHAR(45) NULL,
-     `owner` VARCHAR(45) NULL,
-PRIMARY KEY (`id`));
